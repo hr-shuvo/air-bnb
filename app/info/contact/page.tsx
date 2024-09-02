34a47b02e6294f3a5c245998ce6c7d4fa8aaa3a9
hr-shuvo <h.r.shuvo9@gmail.com>
2024-09-02 22:25:28 +0600

    check like (directory)

---
+++ app/info/contact/page.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link"
+
+function ContactPage() {
+    return (
+      <div>
+        <h1 className="text-7xl">Home Page</h1>
+        <Link href="/" className="text-xl text-blue-500 mt-8">
+        back home
+        </Link>
+      </div>
+    )
+  }
+  
+export default ContactPage
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
+import Link from "next/link"
 
 
-function page() {
+function HomePage() {
   return (
     <div>
       <h1 className="text-7xl">Home Page</h1>
+      <Link href="/about" className="text-xl text-blue-500 mt-8">
+      about page
+      </Link>
     </div>
   )
 }
 
-export default page
+export default HomePage
